96380ed145b438a7a756826e671223e70a9fbb97
Kennet Najbjerg <99kennetn@gmail.com>
2018-09-17 08:51:20 +0200

    adsf

---
+++ Ordinering/ordination/Ordinationtest.java
@@ -0,0 +1,14 @@
+package ordination;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Ordinationtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
