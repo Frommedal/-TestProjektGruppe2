f6ed8a84927d72669abe208dcee0e7ead75e0a0d
frommedalLaptop <frommedalLaptop@10.24.5.84>
2018-09-17 09:16:19 +0200

    testderikkedur

--- Ordinering/ordination/Ordinationtest.java
+++ Ordinering/ordination/Ordinationtest.java
@@ -1,14 +1,33 @@
 package ordination;
 
 import static org.junit.Assert.*;
+import ordination.Ordination;
 
 import org.junit.Test;
 
 public class Ordinationtest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//	private Patient janeJensen = new Patient("121256-0512", "Jane Jensen", 63.4);
+//	private Laegemiddel paracetamol = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+//
+//
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+//	
+//	@Test
+//	public void tc1() {
+//		Ordination o = new Ordination(2015-01-01, 2015-01-12, janeJensen, paracetamol);
+//	}
+//	
+//	
+//	@Test
+//	public void t123c1() {
+//		b.setGrundpræmie(1000);
+//		double res = b.beregnPræmie(18, true, 0);
+//		System.out.println(b.beregnPræmie(18, true, 0));
+//		assertEquals(1187.5, res, 0.001);
+//	}
+//	
+//
+//}
